aebfc07fa6874f7486bef97994c9c66d85f6157a
gentkoshi <155828361+gentkoshi@users.noreply.github.com>
2024-05-26 01:53:29 +0200

    update

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -4,17 +4,19 @@ import "./navbar.scss";
 function Navbar() {
   return (
     <div className="navbar">
-      <div className="logo">
-        <NavLink to="/"><img src="./public/logo.png" alt="Logo" /></NavLink>
-      </div>
-      <div className="link">
-        <NavLink to="/" exact activeClassName="active" id="Home">Home</NavLink>
-        <NavLink to="/About" activeClassName="active" id="About">About Us</NavLink>
-        <NavLink to="/Courses" activeClassName="active" id="Courses">Courses</NavLink>
-        <NavLink to="/Instructors" activeClassName="active" id="Instructors">Instructors</NavLink>
-        <NavLink to="/Blog" activeClassName="active" id="Blog">Blog</NavLink>
-        <NavLink to="/Contact" activeClassName="active" id="Contact">Contact Us</NavLink>
-        <button ><img src="./public/search.png" alt="Search" /></button>
+      <div className="container">
+        <div className="logo">
+          <NavLink to="/"><img src="./public/logo.png" alt="Logo" /></NavLink>
+        </div>
+        <div className="link">
+          <NavLink to="/" exact activeClassName="active" id="Home">Home</NavLink>
+          <NavLink to="/About" activeClassName="active" id="About">About Us</NavLink>
+          <NavLink to="/Courses" activeClassName="active" id="Courses">Courses</NavLink>
+          <NavLink to="/Instructors" activeClassName="active" id="Instructors">Instructors</NavLink>
+          <NavLink to="/Blog" activeClassName="active" id="Blog">Blog</NavLink>
+          <NavLink to="/Contact" activeClassName="active" id="Contact">Contact Us</NavLink>
+          <button ><img src="./public/search.png" alt="Search" /></button>
+        </div>
       </div>
     </div>
   );
