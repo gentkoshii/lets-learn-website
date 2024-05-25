340c0f78099a75537f55efd9d5402db19cde8167
gentkoshi <155828361+gentkoshi@users.noreply.github.com>
2024-05-25 23:00:02 +0200

    navbar and footer update

--- src/Components/footer.jsx
+++ src/Components/footer.jsx
@@ -2,24 +2,27 @@ import "./footer.scss";
 
 function Footer() {
     return (
-      <div>
-        <div className="Logo">
-          <a href="/" className="logo-footer"><img src="./public/logo.png" alt="Logo" /></a>
-          <a href="https://www.facebook.com" className="icons-footer"><img src="./public/facebook.png" alt="Facebook" /></a>
-          <a href="https://www.instagram.com" className="icons-footer"><img src="./public/instagram.png" alt="Instagram" /></a>
-          <a href="https://www.twitter.com" className="icons-footer"><img src="./public/twitter.png" alt="Twitter" /></a>
+      <div className="footer">
+        <div className="logo">
+          <a href="/" className="logo-footer"><img src="./public/logo-footer2.png" alt="Logo" /></a>
+          <div className="icons">
+            <a href="https://www.instagram.com" className="icons-footer"><img src="./public/instagram.png" alt="Instagram" /></a>
+            <a href="https://www.twitter.com" className="icons-footer"><img src="./public/twitter.png" alt="Twitter" /></a>
+            <a href="https://www.facebook.com" className="icons-footer"><img src="./public/facebook.png" alt="Facebook" /></a>
+          </div>
           <p>Porto Education. © 2024. <br /> All Rights Reserved</p>
         </div>
-        <div className="AboutUs">
-          <h2>About Us</h2>
-            <p>- Porto Education</p>
-            <p>- 123 Porto Blvd, Suite 100</p>
-            <p>- New York, NY</p>
-            <p>- Phone: <a href="tel:123-456-7890">123-456-7890</a></p>
-            <p>- Email: <a href="mailto:porto@domain.com">porto@domain.com</a></p>
+        <div className="Information">
+        <div className="About">
+          <h3>About Us</h3>
+            <p>Porto Education</p>
+            <p>123 Porto Blvd, Suite 100</p>
+            <p>New York, NY</p>
+            <p>Phone: <a href="tel:123-456-7890">123-456-7890</a></p>
+            <p>Email: <a href="mailto:porto@domain.com">porto@domain.com</a></p>
         </div>
         <div className="Navigation">
-          <h2>Navigation</h2>
+          <h3>Navigation</h3>
             <a href="/">- Home</a>
             <a href="/About">- About Us</a>
             <a href="/Courses">- Courses</a>
@@ -28,18 +31,20 @@ function Footer() {
             <a href="/Contact">- Contact Us</a>
         </div>
         <div className="Support">
-          <h2>Support</h2>
+          <h3>Support</h3>
             <a href="/">- Documentation</a>
             <a href="/">- Forums</a>
             <a href="/">- Open a Ticket</a>
             <a href="/">- FAQ's</a>
         </div>
         <div className="More">
-          <h2>More</h2>
+          <h3>More</h3>
             <a href="/">- Terms of Use</a>
             <a href="/">- Privacy Policy</a>
         </div>
       </div>
+      </div>
+
     )
   }
   
--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,21 +1,24 @@
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import "./navbar.scss";
+
 function Navbar() {
-    return (
-      <div>
-        <div>
-          <Link href="/"><img src="./public/logo.png" alt="Logo" /></Link>
-        </div>
-        <div>
-          <Link to="/" id="Home">Home</Link>
-          <Link to="/About" id="About">About Us</Link>
-          <Link to="/Courses" id="Courses">Courses</Link>
-          <Link to="/Instructors" id="Instructors">Instructors</Link>
-          <Link to="/Blog" id="Blog">Blog</Link>
-          <Link to="/Contact" id="Contact">Contact Us</Link>
-          <button><img src="./public/search-red.png" alt="Search" /></button>
-        </div>
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <NavLink to="/"><img src="./public/logo.png" alt="Logo" /></NavLink>
       </div>
-    )
-  }
-  
-  export default Navbar
+      <div className="link">
+        <NavLink to="/" exact activeClassName="active" id="Home">Home</NavLink>
+        <NavLink to="/About" activeClassName="active" id="About">About Us</NavLink>
+        <NavLink to="/Courses" activeClassName="active" id="Courses">Courses</NavLink>
+        <NavLink to="/Instructors" activeClassName="active" id="Instructors">Instructors</NavLink>
+        <NavLink to="/Blog" activeClassName="active" id="Blog">Blog</NavLink>
+        <NavLink to="/Contact" activeClassName="active" id="Contact">Contact Us</NavLink>
+        <button ><img src="./public/search.png" alt="Search" /></button>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
