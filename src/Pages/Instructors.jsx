6cbe415bf9b6fce6bb82580001ffa5fdb48fe002
gentkoshi <155828361+gentkoshi@users.noreply.github.com>
2024-05-26 06:35:34 +0200

    Instructor update

--- src/Pages/Instructors.jsx
+++ src/Pages/Instructors.jsx
@@ -1,20 +1,58 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import "./Instructors.scss";
 
-
 function Instructors() {
-    return (
-        <div className="Instructors">
-          <div className="Intro">
-              <h1>Instructors</h1>
-          </div>
-          <div className="links">
-            <Link to="/">Home</Link>
-            <p>&gt;</p>
-            <p>Instructors</p>
-          </div>
+  return (
+    <div className="Instructors">
+      <div className="Intro">
+        <h1>Instructors</h1>
+      </div>
+
+      <div className="wrapper">
+        <div className="links">
+          <Link to="/">Home</Link>
+          <p>&gt;</p>
+          <p>Instructors</p>
         </div>
-    )
-  }
-  
-  export default Instructors
+
+        <div className="instructors-cards">
+          {Array.from({ length: 5 }).map((_, index) => (
+            <div className="card" key={index}>
+              <img src="./public/avatar3.jpg" alt="Avatar" />
+              <div className="details">
+                <p>Full Name</p>
+                <p>Available Courses</p>
+                <p>Average Rating</p>
+                <button id="b">View Course</button>
+                <h2>John Doe</h2>
+                <h3>12</h3>
+                <h3>4.75</h3>
+                <p id="p">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum
+                  incidunt ut quae modi et neque tempore quas corrupti quibusdam
+                  autem ab aut, quis nemo enim voluptas, laborum consectetur, iure
+                  laudantium. <br />
+                  <br /> Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Quisquam vel suscipit aspernatur voluptatibus consectetur ad,
+                  modi deleniti cupiditate sunt, harum aut mollitia explicabo
+                  voluptatum quis culpa, asperiores ullam. Odit, nesciunt.
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="get-started">
+        <div className="container">
+          <h1>Ready to kick-start your career?</h1>
+          <a href="/Courses">
+            <button>GET STARTED NOW</button>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Instructors;
