3ac34da3f0fb23d39ae289d70fe2d6b13cb53c8f
gentkoshi <155828361+gentkoshi@users.noreply.github.com>
2024-05-31 14:22:32 +0200

    Navbar - hamburger menu update

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,7 +1,10 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
 import "./navbar.scss";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="navbar">
       <div className="container">
@@ -16,6 +19,19 @@ function Navbar() {
           <NavLink to="/Blog" activeClassName="active" id="Blog">Blog</NavLink>
           <NavLink to="/Contact" activeClassName="active" id="Contact">Contact Us</NavLink>
           <button ><img src="./public/search.png" alt="Search" /></button>
+          <div className="navbarIcon">
+                    <img src="/public/hamburger-menu.png" alt="Hamburger-Menu" onClick={() => setOpen((prev) => !prev)} />
+                </div>
+                <div className={open ? "navbarMenu active" : "navbarMenu"}>
+                        <>
+                        <NavLink to="/" exact activeClassName="active" id="Home">Home</NavLink>
+                        <NavLink to="/About" activeClassName="active" id="About">About Us</NavLink>
+                        <NavLink to="/Courses" activeClassName="active" id="Courses">Courses</NavLink>
+                        <NavLink to="/Instructors" activeClassName="active" id="Instructors">Instructors</NavLink>
+                        <NavLink to="/Blog" activeClassName="active" id="Blog">Blog</NavLink>
+                        <NavLink to="/Contact" activeClassName="active" id="Contact">Contact Us</NavLink>
+                        </>
+                </div>
         </div>
       </div>
     </div>
